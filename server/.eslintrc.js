2eff367876de7f93c92f513f0021e60e76a5f8e5
Mateusz <kontakt@mateuszoracz.pl>
2016-08-26 20:08:43 +0200

    init

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+     "extends": "eslint:recommended",
+     "plugins": [
+         "node"
+     ],
+     rules: {
+       indent: [
+         'error', 2
+       ],
+       quotes: [
+         'error',
+         'single'
+       ],
+       semi: [
+         'error'
+       ]
+     }
+};
