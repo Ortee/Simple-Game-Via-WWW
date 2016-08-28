b90a54367f2fc748e58537acbca3098fa19822f0
Mateusz <kontakt@mateuszoracz.pl>
2016-08-28 21:03:11 +0200

    simple express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('NEW GAME!');
+var express = require('express');
+var logger = require('winston');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('NEW GAME!');
+});
+
+app.listen(3000, function () {
+  logger.info('Server listening on port 3000!');
+});
